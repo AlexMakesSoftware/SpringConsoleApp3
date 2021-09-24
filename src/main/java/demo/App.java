3d7706cfbbda597fdbd1d6cd0ca9c8618daeaba4
Alex Connell <83471521+AlexMakesSoftware@users.noreply.github.com>
2021-09-24 12:53:13 +0100

    make spring a bit quieter

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -4,16 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContext;
+
 
 /**
  * Simple example of a configurable spring console application.
  */
 @SpringBootApplication(scanBasePackages = {"demo", "service"} )//start scanning from this point in the classpath.
 public class App implements CommandLineRunner {   
-    
+
     @Autowired
-    private ApplicationContext context;
+    ThirdParty sender;
 
     public static void main( String[] args ) {
         new SpringApplicationBuilder()                    
@@ -25,9 +25,6 @@ public class App implements CommandLineRunner {
     public void run(String... args) {
         
         System.out.println("run");
-        
-        //n.b. Autowiring this doesn't work.
-        ThirdParty sender = context.getBean(ThirdParty.class);
         sender.doYourThing();
     }
 }
